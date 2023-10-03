435cf1fab0be0c2bf0dfa962fe2f23c7a71a07a4
Eugen <eugen252009@gmail.com>
2023-10-03 18:19:51 +0200

    Zum Commit vorgemerkte Änderungen: 	geändert:       index.ts

--- 14/index.ts
+++ 14/index.ts
@@ -1,17 +1,31 @@
 export function longestCommonPrefix(strs: string[]): string {
+    if (strs.length === 1) return strs[0]
     let result = ""
-    for (let i = 0; i < strs.length; i++) {
-        const element = strs[i]
-    for (let j = 0; j < strs.length; j++) {
-        const element2 = strs[j]
-           prefixoftwostrings(element,element2) 
-        
-        }
+    result = prefixoftwostrings(strs[0], strs[1])
+    console.log(result)
+    if (strs.length === 2) return result
+    for (let index = 1; index < strs.length; index++) {
+        result = prefixoftwostrings(result, strs[index])
     }
+
     return result
 };
 
 
+
+function prefixoftwostrings(string1: string, string2: string): string {
+    let result = ""
+    for (let index = 0; index < string1.length+1; index++) {
+        const element = string1.slice(0, index);
+        const element2 = string2.slice(0, index)
+        console.log(element,element2)
+        if (element === element2) {
+            result = element.slice(0, index)
+        }
+    }
+    return result
+}
+//
 console.log(
     longestCommonPrefix(["flower", "flow", "flight"])
 )
@@ -19,16 +33,6 @@ console.log(
     longestCommonPrefix([
         "dog", "racecar", "car"]
     ))
+console.log(longestCommonPrefix(["ab", "a"]))
+console.log(longestCommonPrefix(["flower", "flower", "flower", "flower"]))
 
-
-function prefixoftwostrings(string1: string, string2: string): string {
-    let result = ""
-    for (let index = 0; index < string1.length; index++) {
-        const element = string1[index];
-        const element2 = string2[index]
-        if (element === element2){
-        result=element.slice(0,index)
-        }
-    }
-    return result 
-}
