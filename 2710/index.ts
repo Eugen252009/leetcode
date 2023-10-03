9f677e9776e9824a978a5b009a401d4ee15db36a
Eugen <eugen252009@gmail.com>
2023-10-03 18:45:28 +0200

    finished 2710

---
+++ 2710/index.ts
@@ -0,0 +1,15 @@
+console.log(removeTrailingZeros("51230100"))
+console.log(removeTrailingZeros("123"))
+
+function removeTrailingZeros(num: string): string {
+	let result = num
+	for (let index = num.length-1; index >= 0; index--) {
+		const element = num[index];
+		if (element === "0") {
+			result = result.slice(0,-1)
+		} else {
+			return result
+		}
+	}
+	return result
+};
